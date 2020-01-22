8add0682c9f964f2208f74a52a6046c3b75097b7
mabyg1 <Mattias.Bygdeson@cybercom.com>
2020-01-22 11:39:31 +0100

    adjusting layout

--- src/components/Instructions.vue
+++ src/components/Instructions.vue
@@ -16,7 +16,7 @@
         <p>{{this.paragraphs.instructions1}}</p>
         <p>{{this.paragraphs.instructions2}}</p>
 
-        <div class="point-description">
+        <!-- <div class="point-description">
           <div class="column">
             <p><span class="badge positive">+20 p</span> {{this.paragraphs.instructions3}}</p>
             <p><span class="badge positive">+16 p</span> {{this.paragraphs.instructions4}}</p>
@@ -26,11 +26,17 @@
             <p><span class="badge negative">-16 p</span> {{this.paragraphs.instructions5}}</p>
             <p><span class="badge negative">-80 p</span> {{this.paragraphs.instructions6}}</p>
           </div>
-        </div>
+        </div> -->
       </div>
 
       <div class="column">
-        <img src="https://via.placeholder.com/446x297" />
+        <!-- <img src="https://via.placeholder.com/446x297" /> -->
+        <div class="point-description">
+          <p><span class="badge positive">+20 p</span> {{this.paragraphs.instructions3}}</p>
+          <p><span class="badge positive">+16 p</span> {{this.paragraphs.instructions4}}</p>
+          <p><span class="badge negative">-16 p</span> {{this.paragraphs.instructions5}}</p>
+          <p><span class="badge negative">-80 p</span> {{this.paragraphs.instructions6}}</p>
+        </div>
       </div>
     </div>
   </div>
@@ -49,15 +55,15 @@ export default {
 <style lang="scss" scoped>
 .wrapper {
   display: grid;
-  grid-template-columns: 65% auto;
+  grid-template-columns: 50% auto;
   grid-gap: 20px;
 }
 
 .point-description {
-  margin-top: 50px;
+  // margin-top: 50px;
   font-size: 0.9em;
   display: grid;
-  grid-template-columns: 50% 50%;
+  // grid-template-columns: 50% 50%;
   // background: rgba(0,0,0,0.1);
   // padding: 20px;
 
@@ -67,7 +73,7 @@ export default {
   }
   
   .badge {
-    width: 120px;
+    width: 80px;
     text-align: center;
     display: inline-block;
     border-radius: 3px;
